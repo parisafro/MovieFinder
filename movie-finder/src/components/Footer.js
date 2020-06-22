6301ac1f361eaf2919f1b08ee1790790f73fd6cb
Parisa Foroudian <parisafro.pf@gmail.com>
2020-06-22 23:11:14 +0430

    add footer

--- movie-finder/src/components/Footer.js
+++ movie-finder/src/components/Footer.js
@@ -7,18 +7,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 class Footer extends React.Component{
     render() {
         return(
-            <div className="bg-dark" style={{height:'150px'}}>
-                <Container className="text-light">
-                    <Row md={4} style={{padding:'20px'}}>
-                        <Col>1 of 3</Col>
-                        <Col>2 of 3</Col>
-                        <Col>3 of 3</Col>
+            <div className="bg-dark" style={{height:'100px'}}>
+                <Container className="text-light text-center">
+                    <Row lg style={{padding:'20px'}}>
+                        <Col>Terms</Col>
+                        <Col>Privacy</Col>
+                        <Col>Security</Col>
+                        <Col>Status</Col>
+                        <Col>Help</Col>
                     </Row>
-                    <Row style={{padding:'20px'}}>
-                        <Col>1 of 3</Col>
-                        <Col>2 of 3</Col>
-                        <Col>3 of 3</Col>
-                    </Row>
+                    <small>All rights reserved.</small>
                 </Container>
             </div>
 
