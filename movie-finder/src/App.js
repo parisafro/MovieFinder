f11ba1c156b4d65c5d92d9e573e3f100a408b8ac
Parisa Foroudian <parisafro.pf@gmail.com>
2020-06-29 18:57:45 +0430

    Update App.js

--- movie-finder/src/App.js
+++ movie-finder/src/App.js
@@ -1,31 +1,23 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {HomePage,Detail} from './components/Source';
 import { BrowserRouter as Router , Switch ,Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from "axios";
 import { render } from '@testing-library/react';
 
-// class App extends React.Component{
-//   render(){
-//     return(
-//       <Router>
-//         <div>
-//           <Switch>
-//             <Route path="/" exact component={HomePage} />
-//             <Route path="/Detail" component={Detail} />
-//           </Switch>
-//         </div>
-//       </Router>
-//     )
-//   }
-// }
-function App() {
-  return(
-    <div>
-      hello
-    </div>
-  );
-
+class App extends React.Component{
+  render(){
+    return(
+      <Router>
+        <div>
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/Detail" component={Detail} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
 }
 
 export default App;
